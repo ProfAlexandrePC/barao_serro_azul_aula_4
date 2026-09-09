3c5bbcfaef2c39719dbb7fce1dcf097b10cb82c6
ALEXANDRE HENRIQUE SILVA <145609944+ProfAlexandrePC@users.noreply.github.com>
2026-09-09 15:10:42 -0300

    Teste script

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const botao = document.querySelector("#botaoMensagem");
 
 const mensagem = document.querySelector("#mensagemInterativa");
+
+botao.addEventListener("click", mostrarMensagem);
+
+function mostrarMensagem() {
+
+    mensagem.textContent =
+        "Você acabou de criar uma interação com JavaScript!";
+
+}
