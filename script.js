36218392c79a776924d0953945cb05fa8d79ca83
ALEXANDRE HENRIQUE SILVA <145609944+ProfAlexandrePC@users.noreply.github.com>
2026-09-14 20:01:39 -0300

    Ajustes

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-const botoesCurtir =
-document.querySelectorAll(".curtir");
+const botoesCurtir = document.querySelectorAll(".curtir");
 botoesCurtir.forEach(function(botaoCurtir) {
 let curtiu = false;
 botaoCurtir.addEventListener("click", curtir);
 function curtir() {
-const contador =
-botaoCurtir.querySelector("span");
+const contador = botaoCurtir.querySelector("span");
 if (curtiu === false) {
 contador.textContent++;
 curtiu = true;
