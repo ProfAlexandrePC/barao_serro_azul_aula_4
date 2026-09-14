6e12ed040abe42c031fdaf4df7d6e56cc9640822
ALEXANDRE HENRIQUE SILVA <145609944+ProfAlexandrePC@users.noreply.github.com>
2026-09-14 20:36:03 -0300

    Ajustes no código

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const botoesCurtir = document.querySelectorAll(".curtir");
+
 botoesCurtir.forEach(function(botaoCurtir) {
-let curtiu = false;
-botaoCurtir.addEventListener("click", curtir);
-function curtir() {
-const contador = botaoCurtir.querySelector("span");
-if (curtiu === false) {
-contador.textContent++;
-curtiu = true;
-} else {
-contador.textContent--;
-curtiu = false;
-}
-}
+    let curtiu = false;
+
+    botaoCurtir.addEventListener("click", curtir);
+
+    function curtir() {
+        const contador = botaoCurtir.querySelector("span");
+
+        if (curtiu === false) {
+            contador.textContent++;
+            curtiu = true;
+        } else {
+            contador.textContent--;
+            curtiu = false;
+        }
+    }
 });
